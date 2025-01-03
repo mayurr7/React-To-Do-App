c6e7146849fe9c6dfcc92d24b4919836fd93f407
Mayur Tekale <mayurtekale7@gmail.com>
2025-01-03 16:55:15 +0530

    upadte

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import Todo from './components/Todo';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  
+    <Todo/>
     </>
   )
 }
